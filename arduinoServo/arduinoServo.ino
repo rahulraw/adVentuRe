f76f3c2dca827ffb0bdb472b4a9c8860af1f7c18
Jerry Li <jerryyxli@gmail.com>
2015-09-20 08:07:36 -0400

    new method

--- arduinoServo/arduinoServo.ino
+++ arduinoServo/arduinoServo.ino
@@ -1,6 +1,13 @@
 #include <ContinuousServo.h>
 #include <ros.h>
 #include <geometry_msgs/Quaternion.h>
+#include <sensor_msgs/Joy.h>
+
+#define UP 2
+#define DOWN 1
+#define LEFT 3
+#define RIGHT 4
+
 
 ros::NodeHandle nh;
 ContinuousServo s1(9);
@@ -10,11 +17,12 @@ bool first = true;
 double origX,origZ;
 double curX,curZ;
 double nextX,nextZ;
-void adjustAngleCb(const geometry_msgs::Quaternion& q)
+//void adjustAngleCb(const geometry_msgs::Quaternion& q)
+void adjustAngleCb(const sensor_msgs::Joy& joy)
 {
   //x is tilt
   //z is pan
-  if (first){
+  /*if (first){
     origX = q.x * 20;
     origZ = q.z * 20 + 20;
     first = false;
@@ -29,10 +37,16 @@ void adjustAngleCb(const geometry_msgs::Quaternion& q)
     
     curX = nextX;
     curZ = nextZ;    
-  }
+  }*/
+  
+  if (joy.buttons[UP]) s1.step(5);
+  else if (joy.buttons[DOWN]) s1.step(-5);
+  else if (joy.buttons[LEFT]) s2.step(5);
+  else if (joy.buttons[RIGHT]) s2.step(-5);
+  
 }
-ros::Subscriber<geometry_msgs::Quaternion> sub("get_quat", &adjustAngleCb);
-
+//ros::Subscriber<geometry_msgs::Quaternion> sub("get_quat", &adjustAngleCb);
+ros::Subscriber<sensor_msgs::Joy> sub("joy", &adjustAngleCb);
   int curPos1 =0;
   int curPos2 =0;
   int nextPos1;
@@ -57,5 +71,5 @@ void loop() {
     curPos2 = nextPos2;
   }*/
   nh.spinOnce();
-  delay(1);
+  delay(5);
 }
